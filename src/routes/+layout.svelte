f87a4c0b3d1fe35e7554e4c9d13c515f37082990
agungm <agungmarwanto96@gmail.com>
2025-12-28 18:44:23 +0700

    memindahkan font ke static, menambahkan SEO meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,22 @@
 <!-- Ini untuk menambah elemen HTML <head> -->
 <svelte:head>
 	<title>Agung Marwanto</title>
+	<meta name="description" content="Agung Marwanto adalah freelance frontend developer yang fokus membangun website modern, cepat, dan minimalist. Berpengalaman di berbagai bidang dan terus belajar teknologi web." />
+
+  	<meta name="robots" content="index, follow" />
+
+  	<!-- Canonical -->
+  	<link rel="canonical" href="https://agungmarwanto.pages.dev" />
+
+	<!-- Open Graph -->
+  	<meta property="og:type" content="website" />
+ 	<meta property="og:title" content="Agung Marwanto — Frontend Engineer" />
+  	<meta property="og:description" content="Membangun UI web yang modern, cepat, dan elegan." />
+  	<meta property="og:url" content="https://agungmarwanto.pages.dev" />
+  	<meta property="og:image" content="https://agungmarwanto.pages.dev/og-image.png" />
+
+  <!-- Twitter -->
+  <meta name="twitter:card" content="summary_large_image" />
 </svelte:head>
 <!-- Main container global -->
 
