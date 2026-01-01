abf0758f4b9829c001c8fe3f7080a0897790bc9e
agungm <agungmarwanto96@gmail.com>
2026-01-01 16:18:50 +0700

    move project to js

---
+++ src/lib/data/projects.js
@@ -0,0 +1,18 @@
+// src/lib/data/projects.js
+
+export const projects = [
+  {
+    title: "My Resume",
+    url: "/", // Link internal
+    image: "/images/Resume-thumbnail.png", // Pastikan path image dimulai dengan slash '/'
+    description: "A Resume for a Web Development Project: Progress in Motion!",
+    technologies: ["Svelte", "TailwindCss"] // Kosongkan jika tidak ada tags
+  },
+  {
+    title: "Garlid Adventure",
+    url: "https://garlidadventure.com/",
+    image: "/images/Garlid-thumbnail.png",
+    description: "A WordPress Landing Page with WooCommerce for Rental, Laundry, and Repair Services!",
+    technologies: ["WooCommerce"]
+  }
+];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
   // Import data yang baru dibuat
   import { aboutData } from '$lib/data/about.js';
   import { experiences } from '$lib/data/experiences';
+  import { projects } from '$lib/data/projects';
   import { siteConfig } from '$lib/data/site.config'; // Kalau mau pakai link resume dari config
 
   let cleanupGlowEffect;
@@ -180,102 +181,61 @@
       <!-- Main Section -->
       <div>
         <ul class="group/list">
-          <!-- Resume Project -->
-          <li class="mb-12">
-            <div
-              class="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-              <!-- Hover Effect Project -->
-              <div
-                class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-zinc-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0,1)] lg:group-hover:drop-shadow-lg">
-              </div>
-              <!-- Picture Project -->
-              <img
-                class="aspect-video object-cover rounded border-2 border-zinc-200/10 transition group-hover:border-zinc-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
-                src="images/Resume-thumbnail.png" alt="Resume Agung Marwanto" loading="lazy" width="200" height="48"
-                decoding="async">
-              <!-- Desc&Title Project -->
-              <div class="z-10 sm:col-span-6">
-                <h3>
-                  <a class="inline-flex items-baseline font-medium leading-tight text-zinc-200 hover:text-lime-300 focus-visible:text-lime-300 group/link text-base"
-                    href="/">
-                    <span
-                      class="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                    <span>My Resume
-                      <span class="relative inline-block h-4 w-4 overflow-hidden ml-1 align-text-bottom">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                          class="absolute inset-0 h-4 w-4 transition-transform duration-300 ease-in-out group-hover/link:-translate-y-full group-hover/link:translate-x-full">
-                          <path fill-rule="evenodd"
-                            d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                            clip-rule="evenodd"></path>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                          class="absolute inset-0 h-4 w-4 -translate-x-full translate-y-full transition-transform duration-300 ease-in-out group-hover/link:translate-x-0 group-hover/link:translate-y-0">
-                          <path fill-rule="evenodd"
-                            d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                            clip-rule="evenodd"></path>
-                        </svg>
+          {#each projects as project}
+            <li class="mb-12">
+              <div class="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                <div class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-zinc-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0,1)] lg:group-hover:drop-shadow-lg"></div>
+
+                <img
+                  class="aspect-video object-cover rounded border-2 border-zinc-200/10 transition group-hover:border-zinc-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                  src={project.image} 
+                  alt={project.title} 
+                  loading="lazy" 
+                  width="200" 
+                  height="48"
+                      decoding="async">
+
+                <div class="z-10 sm:col-span-6">
+                  <h3>
+                    <a class="inline-flex items-baseline font-medium leading-tight text-zinc-200 hover:text-lime-300 focus-visible:text-lime-300 group/link text-base"
+                      href={project.url} target={project.url.startsWith('http') ? '_blank' : '_self'}> 
+                      <span class="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                  
+                      <span>
+                        {project.title}
+                        <span class="relative inline-block h-4 w-4 overflow-hidden ml-1 align-text-bottom">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="absolute inset-0 h-4 w-4 transition-transform duration-300 ease-in-out group-hover/link:-translate-y-full group-hover/link:translate-x-full">
+                            <path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path>
+                          </svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="absolute inset-0 h-4 w-4 -translate-x-full translate-y-full transition-transform duration-300 ease-in-out group-hover/link:translate-x-0 group-hover/link:translate-y-0">
+                            <path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path>
+                          </svg>
+                        </span>
                       </span>
-                    </span>
-                  </a>
-                </h3>
-                <p>
-                  A Resume for a Web Development Project: Progress in Motion!
-                </p>
+
+                    </a>
+                  </h3>
+              
+                  <p class="mt-2 text-sm leading-normal text-zinc-400">
+                    {project.description}
+                  </p>
+
+                  {#if project.technologies && project.technologies.length > 0}
+                    <ul class="mt-2 flex flex-wrap" aria-label="Technologies used:">
+                      {#each project.technologies as tech}
+                        <li class="mr-1.5 mt-2">
+                          <div class="flex items-center rounded-full bg-lime-400/10 px-3 py-1 text-xs font-medium leading-5 text-lime-300 ">
+                            {tech}
+                          </div>
+                        </li>
+                      {/each}
+                    </ul>
+                  {/if}
+
+                </div>
               </div>
-            </div>
-          </li>
-          <!-- Garlid Project -->
-          <li class="mb-12">
-            <div
-              class="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-              <!-- Hover Effect Project -->
-              <div
-                class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-zinc-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0,1)] lg:group-hover:drop-shadow-lg">
-              </div>
-              <!-- Picture Project -->
-              <img
-                class="aspect-video object-cover rounded border-2 border-zinc-200/10 transition group-hover:border-zinc-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
-                src="images/Garlid-thumbnail.png" alt="Website Garlid Adventure" loading="lazy" width="200" height="48"
-                decoding="async">
-              <!-- Desc&Title Project -->
-              <div class="z-10 sm:col-span-6">
-                <h3>
-                  <a class="inline-flex items-baseline font-medium leading-tight text-zinc-200 hover:text-lime-300 focus-visible:text-lime-300 group/link text-base"
-                    href="https://garlidadventure.com/" target="_blank">
-                    <span
-                      class="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                    <span>Garlid Adventure
-                      <span class="relative inline-block h-4 w-4 overflow-hidden ml-1 align-text-bottom">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                          class="absolute inset-0 h-4 w-4 transition-transform duration-300 ease-in-out group-hover/link:-translate-y-full group-hover/link:translate-x-full">
-                          <path fill-rule="evenodd"
-                            d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                            clip-rule="evenodd"></path>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                          class="absolute inset-0 h-4 w-4 -translate-x-full translate-y-full transition-transform duration-300 ease-in-out group-hover/link:translate-x-0 group-hover/link:translate-y-0">
-                          <path fill-rule="evenodd"
-                            d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                            clip-rule="evenodd"></path>
-                        </svg>
-                      </span>
-                    </span>
-                  </a>
-                </h3>
-                <p>
-                  A WordPress Landing Page with WooCommerce for Rental, Laundry, and Repair Services!
-                </p>
-                <ul class="mt-2 flex flex-wrap" aria-label="Technologies used:">
-                  <li class="mr-1.5 mt-2">
-                    <div
-                      class="flex items-center rounded-full bg-lime-400/10 px-3 py-1 text-xs font-medium leading-5 text-lime-300 ">
-                      WooCommerce
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </li>
+            </li>
+          {/each}
         </ul>
       </div>
     </section>
